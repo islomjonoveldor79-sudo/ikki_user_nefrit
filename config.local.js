396a04dd54e0d683b67b40e2762487560758f653
Eldor <islomjonoveldor79@gmail.com>
2026-05-02 23:03:12 +0500

    Delete config.local.js

--- config.local.js
+++
@@ -1,8 +0,0 @@
-window.TECHSHOP_CONFIG = {
-  SUPABASE_URL: "https://bigpkjupiimfjfethfxz.supabase.co",
-  SUPABASE_KEY: "sb_publishable_Bof7iXPjl_WSq1fceKi_6w_iQhf09cq",
-  IK_URL: "https://ik.imagekit.io/zaveeqnja",
-  IK_PUBLIC_KEY: "public_P73tVHOV8MbhGgBny85iUrMtcL4=",
-  // IK_PRIVATE_KEY — bu yerda bo'lmaydi! Faqat backend/server tomonida ishlatilsin
-  TG_USERNAME: "yourusername"
-};
